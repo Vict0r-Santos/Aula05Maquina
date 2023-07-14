68aa9c6dce23f708d70203845417675f266b80bb
Victor <135838623+Vict0r-Santos@users.noreply.github.com>
2023-07-14 17:24:53 -0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,431 @@
+﻿
+namespace Aula05MaquinadeVendas
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.btnagua = new System.Windows.Forms.Button();
+			this.lblnomeprod1 = new System.Windows.Forms.Label();
+			this.nomeprod2 = new System.Windows.Forms.Label();
+			this.btnsalgadinho = new System.Windows.Forms.Button();
+			this.btnchocolate = new System.Windows.Forms.Button();
+			this.btnmonster = new System.Windows.Forms.Button();
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.button5 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblsaldo = new System.Windows.Forms.Label();
+			this.txtsaldo = new System.Windows.Forms.TextBox();
+			this.label3 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.nomeprod3 = new System.Windows.Forms.Label();
+			this.nomeprod4 = new System.Windows.Forms.Label();
+			this.label7 = new System.Windows.Forms.Label();
+			this.label8 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.pictureBox4 = new System.Windows.Forms.PictureBox();
+			this.pictureBox3 = new System.Windows.Forms.PictureBox();
+			this.pictureBox2 = new System.Windows.Forms.PictureBox();
+			this.pictureBox1 = new System.Windows.Forms.PictureBox();
+			this.pictureBox6 = new System.Windows.Forms.PictureBox();
+			this.pictureBox5 = new System.Windows.Forms.PictureBox();
+			this.pictureBox7 = new System.Windows.Forms.PictureBox();
+			this.groupBox1.SuspendLayout();
+			this.panel1.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// btnagua
+			// 
+			this.btnagua.BackColor = System.Drawing.Color.MediumTurquoise;
+			this.btnagua.Location = new System.Drawing.Point(24, 234);
+			this.btnagua.Name = "btnagua";
+			this.btnagua.Size = new System.Drawing.Size(126, 45);
+			this.btnagua.TabIndex = 0;
+			this.btnagua.Text = "Comprar";
+			this.btnagua.UseVisualStyleBackColor = false;
+			this.btnagua.Click += new System.EventHandler(this.btnagua_Click);
+			// 
+			// lblnomeprod1
+			// 
+			this.lblnomeprod1.AutoSize = true;
+			this.lblnomeprod1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblnomeprod1.ForeColor = System.Drawing.Color.White;
+			this.lblnomeprod1.Location = new System.Drawing.Point(65, 122);
+			this.lblnomeprod1.Name = "lblnomeprod1";
+			this.lblnomeprod1.Size = new System.Drawing.Size(40, 16);
+			this.lblnomeprod1.TabIndex = 4;
+			this.lblnomeprod1.Text = "Agua";
+			// 
+			// nomeprod2
+			// 
+			this.nomeprod2.AutoSize = true;
+			this.nomeprod2.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.nomeprod2.ForeColor = System.Drawing.Color.White;
+			this.nomeprod2.Location = new System.Drawing.Point(214, 122);
+			this.nomeprod2.Name = "nomeprod2";
+			this.nomeprod2.Size = new System.Drawing.Size(77, 16);
+			this.nomeprod2.TabIndex = 5;
+			this.nomeprod2.Text = "Salgadinho";
+			// 
+			// btnsalgadinho
+			// 
+			this.btnsalgadinho.BackColor = System.Drawing.Color.MediumTurquoise;
+			this.btnsalgadinho.Location = new System.Drawing.Point(195, 234);
+			this.btnsalgadinho.Name = "btnsalgadinho";
+			this.btnsalgadinho.Size = new System.Drawing.Size(126, 45);
+			this.btnsalgadinho.TabIndex = 6;
+			this.btnsalgadinho.Text = "Comprar";
+			this.btnsalgadinho.UseVisualStyleBackColor = false;
+			this.btnsalgadinho.Click += new System.EventHandler(this.btnsalgadinho_Click);
+			// 
+			// btnchocolate
+			// 
+			this.btnchocolate.BackColor = System.Drawing.Color.MediumTurquoise;
+			this.btnchocolate.Location = new System.Drawing.Point(24, 465);
+			this.btnchocolate.Name = "btnchocolate";
+			this.btnchocolate.Size = new System.Drawing.Size(126, 45);
+			this.btnchocolate.TabIndex = 7;
+			this.btnchocolate.Text = "Comprar";
+			this.btnchocolate.UseVisualStyleBackColor = false;
+			this.btnchocolate.Click += new System.EventHandler(this.btnchocolate_Click);
+			// 
+			// btnmonster
+			// 
+			this.btnmonster.BackColor = System.Drawing.Color.MediumTurquoise;
+			this.btnmonster.Location = new System.Drawing.Point(195, 465);
+			this.btnmonster.Name = "btnmonster";
+			this.btnmonster.Size = new System.Drawing.Size(126, 45);
+			this.btnmonster.TabIndex = 8;
+			this.btnmonster.Text = "Comprar";
+			this.btnmonster.UseVisualStyleBackColor = false;
+			this.btnmonster.Click += new System.EventHandler(this.btnmonster_Click);
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.BackColor = System.Drawing.Color.DarkCyan;
+			this.groupBox1.Controls.Add(this.pictureBox6);
+			this.groupBox1.Controls.Add(this.pictureBox5);
+			this.groupBox1.Controls.Add(this.label2);
+			this.groupBox1.Controls.Add(this.button5);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Controls.Add(this.lblsaldo);
+			this.groupBox1.Controls.Add(this.txtsaldo);
+			this.groupBox1.Location = new System.Drawing.Point(437, 133);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(284, 310);
+			this.groupBox1.TabIndex = 9;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = ".";
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Font = new System.Drawing.Font("Microsoft Tai Le", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label2.ForeColor = System.Drawing.Color.White;
+			this.label2.Location = new System.Drawing.Point(83, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(118, 21);
+			this.label2.TabIndex = 11;
+			this.label2.Text = "Inserir Dinheiro";
+			// 
+			// button5
+			// 
+			this.button5.Location = new System.Drawing.Point(75, 165);
+			this.button5.Name = "button5";
+			this.button5.Size = new System.Drawing.Size(126, 22);
+			this.button5.TabIndex = 10;
+			this.button5.Text = "Salvar";
+			this.button5.UseVisualStyleBackColor = true;
+			this.button5.Click += new System.EventHandler(this.button5_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label1.ForeColor = System.Drawing.Color.White;
+			this.label1.Location = new System.Drawing.Point(41, 43);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(41, 16);
+			this.label1.TabIndex = 10;
+			this.label1.Text = "Saldo";
+			// 
+			// lblsaldo
+			// 
+			this.lblsaldo.AutoSize = true;
+			this.lblsaldo.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.lblsaldo.ForeColor = System.Drawing.Color.White;
+			this.lblsaldo.Location = new System.Drawing.Point(41, 59);
+			this.lblsaldo.Name = "lblsaldo";
+			this.lblsaldo.Size = new System.Drawing.Size(34, 16);
+			this.lblsaldo.TabIndex = 10;
+			this.lblsaldo.Text = "R$ 0";
+			this.lblsaldo.Click += new System.EventHandler(this.lblsaldo_Click);
+			// 
+			// txtsaldo
+			// 
+			this.txtsaldo.Location = new System.Drawing.Point(44, 126);
+			this.txtsaldo.Name = "txtsaldo";
+			this.txtsaldo.Size = new System.Drawing.Size(197, 20);
+			this.txtsaldo.TabIndex = 0;
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label3.Location = new System.Drawing.Point(64, 138);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(51, 16);
+			this.label3.TabIndex = 12;
+			this.label3.Text = "R$ 3,00";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label4.Location = new System.Drawing.Point(228, 138);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(51, 16);
+			this.label4.TabIndex = 13;
+			this.label4.Text = "R$ 5,00";
+			// 
+			// nomeprod3
+			// 
+			this.nomeprod3.AutoSize = true;
+			this.nomeprod3.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.nomeprod3.ForeColor = System.Drawing.Color.White;
+			this.nomeprod3.Location = new System.Drawing.Point(54, 334);
+			this.nomeprod3.Name = "nomeprod3";
+			this.nomeprod3.Size = new System.Drawing.Size(69, 16);
+			this.nomeprod3.TabIndex = 14;
+			this.nomeprod3.Text = "Chocolate";
+			// 
+			// nomeprod4
+			// 
+			this.nomeprod4.AutoSize = true;
+			this.nomeprod4.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.nomeprod4.ForeColor = System.Drawing.Color.White;
+			this.nomeprod4.Location = new System.Drawing.Point(204, 334);
+			this.nomeprod4.Name = "nomeprod4";
+			this.nomeprod4.Size = new System.Drawing.Size(105, 16);
+			this.nomeprod4.TabIndex = 15;
+			this.nomeprod4.Text = "Monster Energy";
+			// 
+			// label7
+			// 
+			this.label7.AutoSize = true;
+			this.label7.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label7.Location = new System.Drawing.Point(54, 350);
+			this.label7.Name = "label7";
+			this.label7.Size = new System.Drawing.Size(51, 16);
+			this.label7.TabIndex = 16;
+			this.label7.Text = "R$ 4,00";
+			// 
+			// label8
+			// 
+			this.label8.AutoSize = true;
+			this.label8.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label8.Location = new System.Drawing.Point(233, 350);
+			this.label8.Name = "label8";
+			this.label8.Size = new System.Drawing.Size(58, 16);
+			this.label8.TabIndex = 17;
+			this.label8.Text = "R$ 10,00";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.BackColor = System.Drawing.Color.SeaGreen;
+			this.label5.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+			this.label5.ForeColor = System.Drawing.Color.White;
+			this.label5.Location = new System.Drawing.Point(213, 23);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(317, 27);
+			this.label5.TabIndex = 22;
+			this.label5.Text = "Maquina de Vendas do Inferno";
+			// 
+			// panel1
+			// 
+			this.panel1.BackColor = System.Drawing.Color.SeaGreen;
+			this.panel1.Controls.Add(this.pictureBox7);
+			this.panel1.Controls.Add(this.label5);
+			this.panel1.Location = new System.Drawing.Point(-1, -1);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(761, 106);
+			this.panel1.TabIndex = 23;
+			// 
+			// pictureBox4
+			// 
+			this.pictureBox4.Image = global::Aula05MaquinadeVendas.Properties.Resources.pngimg_com___doritos_PNG2;
+			this.pictureBox4.Location = new System.Drawing.Point(217, 157);
+			this.pictureBox4.Name = "pictureBox4";
+			this.pictureBox4.Size = new System.Drawing.Size(84, 71);
+			this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox4.TabIndex = 21;
+			this.pictureBox4.TabStop = false;
+			// 
+			// pictureBox3
+			// 
+			this.pictureBox3.Image = global::Aula05MaquinadeVendas.Properties.Resources._239_2395638_agua_png_garrafa_drinking_water_bottle_png;
+			this.pictureBox3.Location = new System.Drawing.Point(48, 157);
+			this.pictureBox3.Name = "pictureBox3";
+			this.pictureBox3.Size = new System.Drawing.Size(75, 69);
+			this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox3.TabIndex = 20;
+			this.pictureBox3.TabStop = false;
+			// 
+			// pictureBox2
+			// 
+			this.pictureBox2.Image = global::Aula05MaquinadeVendas.Properties.Resources.chocolatepng_parspng_com_7;
+			this.pictureBox2.Location = new System.Drawing.Point(35, 369);
+			this.pictureBox2.Name = "pictureBox2";
+			this.pictureBox2.Size = new System.Drawing.Size(99, 93);
+			this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox2.TabIndex = 19;
+			this.pictureBox2.TabStop = false;
+			// 
+			// pictureBox1
+			// 
+			this.pictureBox1.Image = global::Aula05MaquinadeVendas.Properties.Resources.Monster_Energy;
+			this.pictureBox1.Location = new System.Drawing.Point(236, 369);
+			this.pictureBox1.Name = "pictureBox1";
+			this.pictureBox1.Size = new System.Drawing.Size(43, 90);
+			this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox1.TabIndex = 18;
+			this.pictureBox1.TabStop = false;
+			// 
+			// pictureBox6
+			// 
+			this.pictureBox6.Image = global::Aula05MaquinadeVendas.Properties.Resources.logo_pix_512;
+			this.pictureBox6.Location = new System.Drawing.Point(151, 193);
+			this.pictureBox6.Name = "pictureBox6";
+			this.pictureBox6.Size = new System.Drawing.Size(104, 76);
+			this.pictureBox6.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox6.TabIndex = 13;
+			this.pictureBox6.TabStop = false;
+			// 
+			// pictureBox5
+			// 
+			this.pictureBox5.Image = global::Aula05MaquinadeVendas.Properties.Resources.visa_and_mastercard_logo_26;
+			this.pictureBox5.Location = new System.Drawing.Point(23, 217);
+			this.pictureBox5.Name = "pictureBox5";
+			this.pictureBox5.Size = new System.Drawing.Size(108, 26);
+			this.pictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox5.TabIndex = 12;
+			this.pictureBox5.TabStop = false;
+			// 
+			// pictureBox7
+			// 
+			this.pictureBox7.Image = global::Aula05MaquinadeVendas.Properties.Resources.WhatsApp_svg;
+			this.pictureBox7.Location = new System.Drawing.Point(536, 13);
+			this.pictureBox7.Name = "pictureBox7";
+			this.pictureBox7.Size = new System.Drawing.Size(62, 42);
+			this.pictureBox7.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+			this.pictureBox7.TabIndex = 23;
+			this.pictureBox7.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.Color.MediumSeaGreen;
+			this.ClientSize = new System.Drawing.Size(757, 601);
+			this.Controls.Add(this.pictureBox4);
+			this.Controls.Add(this.pictureBox3);
+			this.Controls.Add(this.pictureBox2);
+			this.Controls.Add(this.pictureBox1);
+			this.Controls.Add(this.label8);
+			this.Controls.Add(this.label7);
+			this.Controls.Add(this.nomeprod4);
+			this.Controls.Add(this.nomeprod3);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.btnmonster);
+			this.Controls.Add(this.btnchocolate);
+			this.Controls.Add(this.btnsalgadinho);
+			this.Controls.Add(this.nomeprod2);
+			this.Controls.Add(this.lblnomeprod1);
+			this.Controls.Add(this.btnagua);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.panel1.ResumeLayout(false);
+			this.panel1.PerformLayout();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+			((System.ComponentModel.ISupportInitialize)(this.pictureBox7)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button btnagua;
+		private System.Windows.Forms.Label lblnomeprod1;
+		private System.Windows.Forms.Label nomeprod2;
+		private System.Windows.Forms.Button btnsalgadinho;
+		private System.Windows.Forms.Button btnchocolate;
+		private System.Windows.Forms.Button btnmonster;
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label lblsaldo;
+		private System.Windows.Forms.TextBox txtsaldo;
+		private System.Windows.Forms.Label label2;
+		private System.Windows.Forms.Button button5;
+		private System.Windows.Forms.Label label3;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label nomeprod3;
+		private System.Windows.Forms.Label nomeprod4;
+		private System.Windows.Forms.Label label7;
+		private System.Windows.Forms.Label label8;
+		private System.Windows.Forms.PictureBox pictureBox1;
+		private System.Windows.Forms.PictureBox pictureBox2;
+		private System.Windows.Forms.PictureBox pictureBox3;
+		private System.Windows.Forms.PictureBox pictureBox4;
+		private System.Windows.Forms.PictureBox pictureBox5;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.PictureBox pictureBox6;
+		private System.Windows.Forms.PictureBox pictureBox7;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aula05MaquinadeVendas
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void lblsaldo_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void btnagua_Click(object sender, EventArgs e)
+		{
+			double valor;
+			double agua = 3;
+			string nome = lblnomeprod1.Text;
+
+			
+			valor = double.Parse(lblsaldo.Text.Replace("R$", ""));
+
+			if (valor >= 3)
+			{
+				double sub = valor - agua;
+				lblsaldo.Text = $"{sub}";
+				MessageBox.Show($"Você comprou {nome}");
+				txtsaldo.Clear();
+			}
+			else
+			{
+				MessageBox.Show("Saldo Insuficiente");
+			}
+		}
+
+		private void button5_Click(object sender, EventArgs e)
+
+		{
+			double valor;
+			double saldo;
+
+			valor = double.Parse(txtsaldo.Text);
+			saldo = double.Parse(lblsaldo.Text.Replace("R$", ""));
+			double soma = saldo + valor;
+
+			lblsaldo.Text = $"R$ {soma}";
+			txtsaldo.Clear();
+		}
+
+		private void btnsalgadinho_Click(object sender, EventArgs e)
+		{
+			double valor;
+			double salgadinho = 5;
+			string nome = nomeprod2.Text;
+
+
+			valor = double.Parse(lblsaldo.Text.Replace("R$", ""));
+
+			if (valor >= 5)
+			{
+				double sub = valor - salgadinho;
+				lblsaldo.Text = $"{sub}";
+				MessageBox.Show($"Você comprou {nome}");
+				txtsaldo.Clear();
+			}
+			else
+			{
+				MessageBox.Show("Saldo Insuficiente");
+			}
+		}
+
+		private void btnchocolate_Click(object sender, EventArgs e)
+		{
+			double valor;
+			double choc = 4;
+			string nome = nomeprod3.Text;
+
+
+			valor = double.Parse(lblsaldo.Text.Replace("R$", ""));
+
+			if (valor >= 4)
+			{
+				double sub = valor - choc;
+				lblsaldo.Text = $"{sub}";
+				MessageBox.Show($"Você comprou {nome}");
+				txtsaldo.Clear();
+			}
+			else
+			{
+				MessageBox.Show("Saldo Insuficiente");
+			}
+		}
+
+		private void btnmonster_Click(object sender, EventArgs e)
+		{
+			double valor;
+			double monster = 10;
+			string nome = nomeprod4.Text;
+
+
+			valor = double.Parse(lblsaldo.Text.Replace("R$", ""));
+
+			if (valor >= 10)
+			{
+				double sub = valor - monster;
+				lblsaldo.Text = $"{sub}";
+				MessageBox.Show($"Você comprou {nome}");
+				txtsaldo.Clear();
+			}
+			else
+			{
+				MessageBox.Show("Saldo Insuficiente");
+			}
+		}
+	}
+}
